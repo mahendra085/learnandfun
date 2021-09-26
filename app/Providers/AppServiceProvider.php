5bdb9955da25f1dd1fcb0be19d8b4ede0b055fe7
mahendra085 <mahendra.singh089@gmail.com>
2021-09-26 19:39:52 +0530

    removed comment from app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,11 +16,7 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+    
     public function register()
     {
         //
